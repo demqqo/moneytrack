eb8b813dacae414829a9d9e6fd9c3fabc7440db4
demqqo <138254074+demqqo@users.noreply.github.com>
2024-04-10 21:56:15 -0400

    UI of total income and expense
    UI of total adding

---
+++ src/components/total.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Total = () => {
+  return (
+    <div className="info-box pt-3 pb-2">
+      <div className="row text-center ">
+        <div className="col border-end"><p className="fw-semibold">INCOME</p>
+        <p className="green">$500.00</p></div>
+        <div className="col "><p className="fw-semibold">EXPENSE</p><p className="red">$240.00</p></div>
+      </div>
+    </div>
+  )
+}
+
+export default Total
