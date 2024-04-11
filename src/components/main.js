e267ea63ecd4552325071a056d378f0c9100b540
demqqo <138254074+demqqo@users.noreply.github.com>
2024-04-10 21:53:40 -0400

    created main

---
+++ src/components/main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Total from './total'
+import History from "./history"
+import Newtransaction from "./newtransaction"
+
+const Main = () => {
+  return (
+    <div className='mt-5'>
+      <p className="fw-semibold">YOUR BALANCE</p>
+      <h1>$260.00</h1>
+    </div>
+  )
+}
+
+export default Main
