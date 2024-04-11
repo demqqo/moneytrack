9e188c870fb432bab5442484b062f352451dcf16
demqqo <138254074+demqqo@users.noreply.github.com>
2024-04-10 21:52:56 -0400

    add to app components "header" "main"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
-
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./components/header"
+import Main from "./components/main"
 function App() {
   return (
-    <div className="App">
+    <div>
+  <Header />
+    <div className="container">
       
-    </div>
+      <Main />
+    </div></div>
   );
 }
 
