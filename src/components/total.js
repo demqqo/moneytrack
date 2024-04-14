f858bbf5dc686073b87c4a7e2ffde7814e0307f4
demqqo <138254074+demqqo@users.noreply.github.com>
2024-04-14 00:49:43 -0400

    shows income and expense
    with filter of array from GlobalContext it add positive amounts and
    subtract negative

--- src/components/total.js
+++ src/components/total.js
@@ -1,12 +1,16 @@
-import React from 'react'
-
+import React , {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
 const Total = () => {
+const {transactions} = useContext(GlobalContext);
+const amounts = transactions.map(transaction=>transaction.amount)
+const income = amounts.filter(item=> item >0).reduce((acc, item) => (acc+=item),0).toFixed(2);
+const expense = amounts.filter(item => item < 0).reduce((acc,item) => (acc-=item),0).toFixed(2);
   return (
-    <div className="info-box pt-3 pb-2">
+    <div className="info-box pt-4 pb-3">
       <div className="row text-center ">
         <div className="col border-end"><p className="fw-semibold">INCOME</p>
-        <p className="green">$500.00</p></div>
-        <div className="col "><p className="fw-semibold">EXPENSE</p><p className="red">$240.00</p></div>
+        <p className="green fs-4 fw-medium">+${income}</p></div>
+        <div className="col "><p className="fw-semibold">EXPENSE</p><p className="red fs-4 fw-medium">-${expense}</p></div>
       </div>
     </div>
   )
