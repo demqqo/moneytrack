25eca4fd396787cc334636a2e658f9bcff183320
demqqo <138254074+demqqo@users.noreply.github.com>
2024-04-14 00:53:40 -0400

    render of transaction
    it checks if it is expense or income and render text and amount from GlobalContext

--- src/components/transaction.js
+++ src/components/transaction.js
@@ -1,3 +1,26 @@
+import React, {useContext}  from 'react'
+import "../App.css"
+import { GlobalContext } from '../context/GlobalState'
+
+const Transaction = ({transaction}) => {
+    
+const { deleteTransaction } = useContext(GlobalContext);
+const sign = transaction?.amount < 0 ? '-' : '+';
+
+  return (
+    
+    <div>
+      <div className={(transaction?.amount < 0 ? 'red-box li' : 'green-box li') } >
+          <div className="info-box d-flex ">
+            <div className="me-auto p-2">{transaction?.text || 'Unknown'}</div> 
+            <div className="p-2">{sign}${Math.abs(transaction?.amount) || 0}</div>
+            
+          </div>
         </div>
         <button onClick={()=>deleteTransaction(transaction?.id)} className="delete-btn">X</button>
     </div>
+    
+  )
+}
+
+export default Transaction
