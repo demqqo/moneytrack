f50013ebaccbe269e09093e405ebea41157a23e0
demqqo <138254074+demqqo@users.noreply.github.com>
2024-04-14 00:52:04 -0400

    moved creation of list element in separate component
    by using array it will generate all object's items in the list

--- src/components/history.js
+++ src/components/history.js
@@ -1,29 +1,17 @@
-import React from 'react'
-
+import React , {useContext} from 'react'
+import Transaction from './transaction'
+import { GlobalContext } from '../context/GlobalState'
 const History = () => {
+
+const {transactions} = useContext(GlobalContext);
+
   return (
     <div className="mt-4">
       <h3>History</h3>
-      <div className="container"><div className=" green-box">
-            <div className="info-box d-flex mb-3">
 
-            <div className="me-auto p-2">Cash</div> <div className="p-2">+500</div>
-            </div>
-           
-            </div><div className=" red-box">
-            <div className="info-box d-flex mb-3">
-
-            <div className="me-auto p-2">Book</div> <div className="p-2">-40</div>
-            
-           </div>
-            </div>
-            <div className=" red-box">
-            <div className="info-box d-flex mb-3">
-
-            <div className="me-auto p-2">Camera</div> <div className="p-2">-200</div>
-            
-           </div>
-            </div></div>
+      <div className="container list">
+      {transactions.map(transaction=>(<Transaction key={transaction.id} transaction={transaction}/>))}
+    </div>
     </div>
   )
 }
