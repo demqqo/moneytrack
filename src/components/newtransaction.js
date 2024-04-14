3316c97edf666214d7960539b36bc93b3befd4b1
demqqo <138254074+demqqo@users.noreply.github.com>
2024-04-14 00:55:15 -0400

    adding of new transaction
    add users input to the GlobalContext, we use action ADD_TRANSACTION

--- src/components/newtransaction.js
+++ src/components/newtransaction.js
@@ -1,19 +1,37 @@
-import React from 'react'
-
+import React, {useState, useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
 const Newtransaction = () => {
+  const [text, setText] = useState("")
+  const [amount, setAmount] = useState("")
+  const {addTransaction} = useContext(GlobalContext);
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+   
+const newTransaction= {
+  
+  id: Math.floor(Math.random()*100000000),
+  text,
+  amount: +amount
+  }
+  
+    addTransaction(newTransaction);
+    setText("");
+        setAmount("");
+  }
   return (
     <div>
 
       <h3>Abb New transaction</h3>
       <div className="container">
       <div>Text</div>
-      <input className="mt-2 mb-2 p-2" type="text" placeholder="Enter text..."></input>
+         <form onSubmit={handleSubmit}><input className="mt-2 mb-2 p-2" type="text" placeholder="Enter text..." value={text}onChange={(e)=>setText(e.target.value)}></input>
       <div >Amount</div>
       <div>(negative -expense, positive -income)</div>
-      <input className="mt-2 mb-2 p-2" type="text" placeholder="Enter amount..."></input>
-      <input className="mt-2 mb-2 p-2 button" type="submit"value="Add transaction"></input></div>
+   
+      <input className="mt-2 mb-2 p-2" type="text" placeholder="Enter amount..." value={amount}onChange={(e)=>setAmount(e.target.value)}></input>
+      <input className="mt-2 mb-2 p-2 button" type="submit"value="Add transaction"></input></form></div>
     </div>
   )
-}
+  }
 
 export default Newtransaction
